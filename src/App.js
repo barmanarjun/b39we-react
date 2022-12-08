3d42fbac3fb5bf613e072ae32faf522612fb72b6
ArjunBarman <MY_NAME@example.com>
2022-12-08 10:03:15 +0530

    Welcome component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { useState } from "react";
 
 function App() {
+  // let name = "Arjun";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome
+        name="Arjun"
+        pic="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
+      <Welcome
+        name="Arun"
+        pic="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
+      <Welcome
+        name="Barman"
+        pic="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      />
+      {/* <h1>
+        Hi, {name}🎉🎉 - {100 * 2} followers
+      </h1> */}
     </div>
   );
 }
-
+function Welcome({ pic, name }) {
+  // let name = "Barman";
+  return (
+    <div className="App">
+      <img classname="user-pic" src={pic} alt={name}></img>
+      <h1>
+        Hi, {name}🎉🎉 - {100 * 2} followers
+      </h1>
+    </div>
+  );
+}
+// function Welcome1(props) {
+//   // let name = "Barman";
+//   const { pic, name } = props;
+//   return (
+//     <div className="App">
+//       <img classname="user-pic" src={pic} alt={name}></img>
+//       <h1>
+//         Hi, {props.name}🎉🎉 - {100 * 2} followers
+//       </h1>
+//     </div>
+//   );
+// }
+// Component Declaration = View + Logic`
+// function Welcome(props) {
+//   const { pic, name } = props;
+//   return (
+//     <div>
+//       <img className="user-pic" src={pic} alt={name} />
+//       <h1>Hi, {name}🎉🎉</h1>
+//     </div>
+//   );
+// }
+// function counter() {
+//   let [like, setLike] = useState(0);
+//   return (
+//     <div>
+//       <button type="" onClick={() => setLike(like + 1)}>
+//         👍Like
+//       </button>
+//     </div>
+//   );
+//   const likeStyle = {
+//     color:"green"
+//   };
+// }
+// function counter1() {
+//   let [dislike, setdisLike] = useState(0);
+//   return (
+//     <div>
+//       <button type="" onClick={() => setdisLike(dislike + 1)}>
+//         👎disLike
+//       </button>
+//     </div>
+//   );
+//   const dislikeStyle = {
+//     color: "red",
+//   };
+// }
 export default App;
---
+++ src/notes.md
@@ -0,0 +1,29 @@
+### Intro to React
+
+1. JSX (JavaScript XML) --> JS (Webpack + babel)
+2. Inside return() is JSX
+
+### why classname?
+
+1. class is a reserved keyword in JS
+2. for --> htmlFor in JSX
+
+### Handy shortcuts
+
+1. ctrl + ` --> show / hide terminal
+2. ctrl + , --> settings | format on save
+3. ctrl + c --> stop react server in terminal
+4. win + . --> emojis
+
+### function to be considered as components
+
+1. function should start with a capital letter
+2. It should at least have one jsx element
+
+### props --> arguments
+
+1. props --> properties of the components
+
+### DRY -->
+
+Don't Repeat Yourself
